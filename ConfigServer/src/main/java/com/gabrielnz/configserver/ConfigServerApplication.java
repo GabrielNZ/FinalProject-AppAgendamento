135586622c6b78d812666b07b3959b324769634e
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-10-27 21:40:41 -0300

    Creating and preparing the MS-projects

---
+++ ConfigServer/src/main/java/com/gabrielnz/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielnz.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ DisponibilidadeOcupada/src/main/java/com/gabrielnz/disponibilidadeocupacada/DisponibilidadeOcupacadaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielnz.disponibilidadeocupacada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisponibilidadeOcupacadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisponibilidadeOcupacadaApplication.class, args);
+    }
+
+}
